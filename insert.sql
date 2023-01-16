8a3a5bb75b246909d2b4db4cc778509440f72215
rizzleduq <47604621+rizzleduq@users.noreply.github.com>
2023-01-16 10:39:00 +0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,126 @@
+insert into posts (title)
+values ('guide');
+insert into posts (title)
+values ('resotrer');
+insert into posts (title)
+values ('artist');
+insert into posts (title)
+values ('warden');
+insert into posts (title)
+values ('guard');
+insert into posts (title)
+values ('guide1');
+
+
+insert into staff (last_name, salary, id_post)
+values ('kilmurry', 23, 1);
+insert into staff (last_name, salary, id_post)
+values ('bromley', 48, 1);
+insert into staff (last_name, salary, id_post)
+values ('perotti', 94, 1);
+insert into staff (last_name, salary, id_post)
+values ('littledyke', 66, 3);
+insert into staff (last_name, salary, id_post)
+values ('turrill', 28, 3);
+insert into staff (last_name, salary, id_post)
+values ('batrip', 2, 4);
+insert into staff (last_name, salary, id_post)
+values ('mollitt', 2, 4);
+insert into staff (last_name, salary, id_post)
+values ('ekless', 99, 1);
+insert into staff (last_name, salary, id_post)
+values ('ekless2', 99, 1);
+insert into staff (last_name, salary, id_post)
+values ('perotti', 94, 1);
+
+
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('photolist', 10, 2);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('snaptags', 5, 3);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('trilith', 4, 3);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('zoonder', 5, 4);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('blognation', 3, 3);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('skinte', 20, 2);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('skinder', 10, 1);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('trilia', 6, 4);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('trilia2', 2, 6);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('africa', 3, 3);
+insert into museum (name_of_museum, amount_of_rooms, amount_of_floors)
+values ('zone', 2, 1);
+
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (1, '4/2/2020', '26/12/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (2, '31/5/2020', '19/8/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (3, '7/2/2020', '24/5/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (4, '8/1/2020', '23/5/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (5, '3/5/2020', '9/6/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (6, '25/11/2020', '7/10/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (7, '11/3/2020', '23/11/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (7, '11/2/2020', '23/11/2021');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (8, '18/6/2020', '19/10/2020');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (8, '18/1/2021', '19/4/2021');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (8, '18/2/2021', '19/3/2021');
+insert into exhibition (id_mus, date_begin_ex, date_end_ex)
+values (2, '18/1/2020', '19/1/2020');
+
+
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (1, 10, 10, 5);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (2, 11, 10, 48);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (3, 12, 5, 12);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (4, 13, 9, 11);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (5, 14, 5, 24);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (6, 15, 3, 33);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (7, 16, 3, 17);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (8, 17, 8, 58);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (9, 17, 8, 58);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (10, 17, 8, 59);
+insert into rooms (id_mus, num_of_room, amount_of_pers, capacity)
+values (11, 16, 7, 60);
+
+
+
+insert into excursions (duration, price, id_rab, id_room)
+values ('12:33 am', 61, 1, 1);
+insert into excursions (duration, price, id_rab, id_room)
+values ('3:13 pm', 55, 2, 2);
+insert into excursions (duration, price, id_rab, id_room)
+values ('7:16 pm', 12, 3, 3);
+insert into excursions (duration, price, id_rab, id_room)
+values ('7:26 am', 23, 4, 4);
+insert into excursions (duration, price, id_rab, id_room)
+values ('2:13 am', 99, 5, 5);
+insert into excursions (duration, price, id_rab, id_room)
+values ('3:08 am', 96, 6, 6);
+insert into excursions (duration, price, id_rab, id_room)
+values ('7:50 pm', 91, 7, 7);
+insert into excursions (duration, price, id_rab, id_room)
+values ('1:29 am', 45, 8, 8);
